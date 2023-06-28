5e33f93af108355f92dbb6b5c32ef57a9464d92d
Sivasanthosh07 <86644164+Sivasanthosh07@users.noreply.github.com>
2023-06-28 14:14:56 +0530

    Update backup_jenkins.py

--- dags/backup_jenkins.py
+++ dags/backup_jenkins.py
@@ -12,7 +12,7 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 
 
 default_args={
-    "owner":"santhosh",
+    "owner":"app-migration",
     "retries":5,
     "retry_delay":timedelta(minutes=3)
 
